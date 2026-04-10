363344a42fe343956098ecc04281334c036b62c2
Matias_Soria <matiassoriagarcia.15@gmail.com>
2026-04-10 08:24:08 -0500

    RESOLVER TAREA

---
+++ actividad 1 .py
@@ -0,0 +1,23 @@
+# Matias Soria, 8/04/2026
+
+print ("La suma es: ", 3 + 4)
+print ("La resta es: ", 3 - 4)
+print ("La multiplicación es: ", 3 * 4)
+print ("El módulo es: ", 3 % 4)
+print ("La división es: ", 3 / 4)
+print ("La potencia es: ", 3 ** 4)
+print ("La división entera es: ", 3 // 4)
+
+print ("Mi nombre es Matias")
+print ("Mi apellido es Soria")
+print ("Mi país es Ecuador")
+print ("Estoy disfrutando de 30 dias de python")
+
+print (type(10))
+print (type(9.8))
+print (type(3.14))
+print (type(4 - 4j))
+print (type(['Asabeneh', 'Python', 'Finland']))
+print (type("¿Tu nombre"))
+print (type("Tu apellido de familia"))
+print (type("Tu país"))
---
+++ actividad 2_variables .py
@@ -0,0 +1,24 @@
+#Matias Soria 8/04/2026
+""" primer_nombre, primer_apellido, pais, edad, estado_civil = 'Matias' , 'Soria' , 'Ecuador' , '16' , 'soltero'
+print (primer_nombre, primer_apellido, edad, estado_civil)
+print ('primer_nombre:', primer_nombre)
+print ('primer_apellido:', primer_apellido)
+print ('edad:', edad)
+print ('estado_civil:', estado_civil)"""
+
+#Matias Soria, 10/04/2026 EJERCICIOS VARIABLES
+
+nombre = "Matias"
+apellido = "Soria"
+nombreCompleto = nombre + apellido
+pais = "Ecuador"
+ciudad = "Quito"
+edad = 16
+anio = 2026
+estaCasado = False
+esVerdadero = True
+luzEncendida = False
+
+# Declarar varias variables en una sola línea
+x, y, z = "uno", "dos", "tres"
+
