9ed813420f25fb7c387b048007bc4dfb7946504c
Matias_Soria <matiassoriagarcia.15@gmail.com>
2026-04-15 13:27:03 -0500

    hacer tarea

---
+++ actividad 4 (strings) .py
@@ -0,0 +1,10 @@
+print ('hola mundo \n chao')             
+
+nombre = 'Matias'
+print (f'El número de letras de mi nombre es: {len(nombre)}')
+a,b,c,d,e,f = nombre
+print (a,b,c,d,e,f)
+first_letter = nombre[0]
+print (first_letter)        
+second_letter = nombre[1]
+print (second_letter)
