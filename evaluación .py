aacbbc7487a1d17140cd45a43f78f7ad133bde57
Matias_Soria <matiassoriagarcia.15@gmail.com>
2026-04-20 09:27:19 -0500

    creación evaluación

---
+++ evaluación .py
@@ -0,0 +1,19 @@
+# ===== PARTE A =====
+
+# Respuesta 1:
+
+# Respuesta 2:
+
+# ...
+
+# ===== PARTE B =====
+
+# Código corregido
+
+# ...
+
+# ===== PARTE C =====
+
+# Programa integrador
+
+# ...
