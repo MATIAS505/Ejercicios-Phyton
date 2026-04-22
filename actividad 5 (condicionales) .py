bfecfd76b13c0b3e8a35208ec23f51a7ff1a9fab
Matias_Soria <matiassoriagarcia.15@gmail.com>
2026-04-22 13:20:32 -0500

    act 5

---
+++ actividad 5 (condicionales) .py
@@ -0,0 +1,40 @@
+'''a = int (input("Ingrese un número: "))
+
+if a > 0 and a%2 == 0:
+
+    print("es un número positivo y par")
+elif a > 0 :
+    print("es un número positivo e impar")
+elif a< 0 and a%2 == 0:
+    print("es un número negativo y par")
+elif a < 0:
+    print("es un número negativo e impar")
+else:
+    print("el número es cero")'''
+
+
+'''numero = int(input("Numero: "))
+if numero != 0:
+    if numero > 0:
+        print("El numero es positivo")
+        if numero % 2 == 0:
+            print("El numero es par")
+        else:
+            print("El numero es impar")
+    else:
+        print("El numero es negativo")
+        if numero % 2 != 0:
+            print("El numero es impar")
+        else :
+            print("El numero es par")
+else:
+    print("El numero es cero")'''
+
+
+
+
+edad = int(input("Ingrese su edad: "))
+if edad >= 18:
+    print("Usted es mayor de edad")
+else:
+    print("Usted es menor de edad")
